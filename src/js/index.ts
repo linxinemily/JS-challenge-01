2728b78ce8daf9746c791244f4793467917d12a5
linxin <you@example.com>
2019-04-28 01:56:02 +0800

    remove bootstrap

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,6 +1,6 @@
 import '../scss/style.scss';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 // data
 const grid : HTMLElement = document.querySelector('.grid')
