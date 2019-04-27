50ba9bd291827240dac15bf1684648109f9b4a92
linxin <you@example.com>
2019-04-27 18:35:34 +0800

    fix some style

--- src/js/index.ts
+++ src/js/index.ts
@@ -14,20 +14,11 @@ for (let i = 2; i < 11; i++) {
 function addNewElement(startNum : number) {
     let newCell : HTMLElement = document.createElement('div')
     newCell.className = 'grid-cell'
-    newCell.innerHTML = `<div class="grid-cell-left">
-        <div class="title">${startNum}</div>
-        <div>${startNum} x 1 = ${startNum * 1 }</div>
-        <div>${startNum} x 2 = ${startNum * 2 }</div>
-        <div>${startNum} x 3 = ${startNum * 3 }</div>
-    </div>
-    <div class="grid-cell-right">
-        <div>${startNum} x 4 = ${startNum * 4 }</div>
-        <div>${startNum} x 5 = ${startNum * 5 }</div>
-        <div>${startNum} x 6 = ${startNum * 6 }</div>
-        <div>${startNum} x 7 = ${startNum * 7 }</div>
-        <div>${startNum} x 8 = ${startNum * 8 }</div>
-        <div>${startNum} x 9 = ${startNum * 9 }</div>
-    </div>`
+    let str :string = `<div class="title">${startNum}</div>`
+    for (let i = 1; i < 10; i++) {
+        str += `<div>${startNum} x 1 = ${startNum * i }</div>`
+    }
+    newCell.innerHTML = str
     grid.insertBefore(newCell, titleCell.nextSibling)
 }
 for (let i = 9; i > 1; i--) {
